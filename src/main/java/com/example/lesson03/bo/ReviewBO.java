e22db4bcb8aecc96a54638e05b2865acf9b5b911
DESKTOP-O7E5F2L\user <test@test.com>
2024-06-18 18:50:54 +0900

    2024-06-18 수업

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -35,5 +35,10 @@ public class ReviewBO {
 	public int updateReviewById(int id,String review) {
 		return reviewMapper.updateReviewById(id, review);
 	}
-
+	
+	//  input: id
+	// output: 
+	public void deleteReviewById(int id) {
+	   reviewMapper.deleteReviewById(id); 
+	}
 }
